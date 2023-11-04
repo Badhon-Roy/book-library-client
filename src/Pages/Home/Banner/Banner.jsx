0b96522ea4073e40341cedbdaadc0396fc13213e
Badhon-Roy <roybadhon286@gmail.com>
2023-11-04 23:27:47 +0600

    slider added in the home route

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,86 @@
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css';
+
+const Banner = () => {
+    return (
+        <div className='max-w-[1200px] mx-auto md:px-5'>
+        <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            modules={[Navigation]}
+            navigation
+        >
+            <SwiperSlide>
+                <div className="md:h-[80vh] h-[60vh] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("https://d4804za1f1gw.cloudfront.net/wp-content/uploads/sites/50/2018/11/hero.jpg")' }}>
+                    <div className=" bg-[rgba(0,0,0,0.4)] flex md:justify-start justify-center items-center h-full lg:px-32 md:px-16 px-8">
+                        <div className='md:w-2/3'>
+                            <h2 className="text-2xl md:text-4xl md:text-left text-center lg:text-6xl font-bold mb-4 md:mb-8 text-white">
+                            Books Your Bridge to Success
+                            </h2>
+                            <p className="text-white md:text-left md:text-xl md:font-bold ">It encapsulates the idea that reading is a transformative journey, where knowledge serves as the bridge to achieving one is aspirations.</p>
+                            <button className='red_bg hover:bg-blue-600 py-2 px-4 normal-case rounded-md text-white mt-4'>Learn more</button>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className="md:h-[80vh] h-[60vh] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("https://png.pngtree.com/background/20230522/original/pngtree-an-old-style-library-with-shelves-full-of-books-picture-image_2693166.jpg")' }}>
+                    <div className="bg-[rgba(0,0,0,0.4)] flex md:justify-start justify-center items-center h-full lg:px-32 md:px-16 px-8">
+                    <div className='md:w-2/3'>
+                            <h2 className="text-2xl md:text-4xl md:text-left text-center lg:text-6xl font-bold mb-4 md:mb-8 text-white">
+                            Books Your Bridge to Success
+                            </h2>
+                            <p className="text-white md:text-left md:text-xl md:font-bold ">It encapsulates the idea that reading is a transformative journey, where knowledge serves as the bridge to achieving one is aspirations.</p>
+                            <button className='red_bg hover:bg-blue-600 py-2 px-4 normal-case rounded-md text-white mt-4'>Learn more</button>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className="md:h-[80vh] h-[60vh] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxNc8ylAN1cVjpaUw8sW2-xVdi46jdigXJ5w&usqp=CAU")' }}>
+                    <div className="bg-[rgba(0,0,0,0.4)] flex md:justify-start justify-center items-center h-full lg:px-32 md:px-16 px-8">
+                    <div className='md:w-2/3'>
+                            <h2 className="text-2xl md:text-4xl md:text-left text-center lg:text-6xl font-bold mb-4 md:mb-8 text-white">
+                            Books Your Bridge to Success
+                            </h2>
+                            <p className="text-white md:text-left md:text-xl md:font-bold ">It encapsulates the idea that reading is a transformative journey, where knowledge serves as the bridge to achieving one is aspirations.</p>
+                            <button className='red_bg hover:bg-blue-600 py-2 px-4 normal-case rounded-md text-white mt-4'>Learn more</button>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className="md:h-[80vh] h-[60vh] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3v87bXPeBGGfrPjNHz6G5yx1zVkAwllhsJg&usqp=CAU")' }}>
+                    <div className="bg-[rgba(0,0,0,0.4)] flex md:justify-start justify-center items-center h-full lg:px-32 md:px-16 px-8">
+                    <div className='md:w-2/3'>
+                            <h2 className="text-2xl md:text-4xl md:text-left text-center lg:text-6xl font-bold mb-4 md:mb-8 text-white">
+                            Books Your Bridge to Success
+                            </h2>
+                            <p className="text-white md:text-left md:text-xl md:font-bold ">It encapsulates the idea that reading is a transformative journey, where knowledge serves as the bridge to achieving one is aspirations.</p>
+                            <button className='red_bg hover:bg-blue-600 py-2 px-4 normal-case rounded-md text-white mt-4'>Learn more</button>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className="md:h-[80vh] h-[60vh] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1FrFO9WoIqpZDPEYYthiT6u8gQCBtTmnk6qJzl-eacCeDQKXFJvEnlrM8e8Tv1l16XVo&usqp=CAU")' }}>
+                    <div className="bg-[rgba(0,0,0,0.4)] flex md:justify-start justify-center items-center h-full lg:px-32 md:px-16 px-8">
+                    <div className='md:w-2/3'>
+                            <h2 className="text-2xl md:text-4xl md:text-left text-center lg:text-6xl font-bold mb-4 md:mb-8 text-white">
+                            Books Your Bridge to Success
+                            </h2>
+                            <p className="text-white md:text-left md:text-xl md:font-bold ">It encapsulates the idea that reading is a transformative journey, where knowledge serves as the bridge to achieving one is aspirations.</p>
+                            <button className='red_bg hover:bg-blue-600 py-2 px-4 normal-case rounded-md text-white mt-4'>Learn more</button>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
+        </Swiper>
+    </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "./Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-            <h2>tHIS ISH home</h2>
+            <Banner></Banner>
         </div>
     );
 };
